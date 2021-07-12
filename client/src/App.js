279a57f32bf9acbfe1074333170576ecf6291ed0
Catalina Williams <webdevcatalina@gmail.com>
2021-07-12 14:17:39 -0600

    search box bringing up api results

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import { React, useState, useEffect } from "react";
 function App() {
 
   const [api, setApi] = useState({});
+  let [city, setCity] = useState('');
+  const uriEncodedCity = encodeURIComponent(city);
 
-  const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=denver&appid=e85d65f27c4dba6f1470e1cee950c874"
+  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${uriEncodedCity}&appid=e85d65f27c4dba6f1470e1cee950c874`
   useEffect(() => {
 
     fetch(apiUrl)
@@ -16,12 +18,25 @@ function App() {
 
   })
 
-console.log(api)
+
 
   return (
     <div className="App">
       {/* <input name="city" onChange={handleChange} value={search.city}></input> */}
      {/* <text>weather description goes here</text> */}
+   
+                <input
+                    type="text"
+                    placeholder="Enter City"
+                    maxLength="50"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    />
+                
+
+             
+
+           
      <h1>{JSON.stringify(api)}</h1>
     </div>
   );
