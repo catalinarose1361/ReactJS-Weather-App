59a4fd1fa1fd112d362ba22e49a1cabf10cfd05e
Catalina Williams <webdevcatalina@gmail.com>
2021-07-16 15:42:26 -0600

    added a form with user input to forecast.js

--- client/src/components/Forecast/Forecast.js
+++ client/src/components/Forecast/Forecast.js
@@ -7,9 +7,19 @@ const Forecast = () => {
     //{"coord":{"lon":-104.9847,"lat":39.7392},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":305.65,"feels_like":303.49,"temp_min":296.88,"temp_max":307.95,"pressure":1009,"humidity":18},"visibility":10000,"wind":{"speed":0.45,"deg":69,"gust":1.34},"clouds":{"all":100},"dt":1626468937,"sys":{"type":2,"id":2004334,"country":"US","sunrise":1626435920,"sunset":1626488779},"timezone":-21600,"id":5419384,"name":"Denver","cod":200}
     let [responseObj, setResponseObj] = useState({});
 
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=denver&appid=e85d65f27c4dba6f1470e1cee950c874`
+    let [city, setCity] = useState('');
 
-   function getForecast() {
+    let [unit, setUnit] = useState('imperial');
+
+    const uriEncodedCity = encodeURIComponent(city)
+
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=${unit}&q=${uriEncodedCity}&appid=e85d65f27c4dba6f1470e1cee950c874`
+
+   function getForecast(e) {
+
+    // PREVENTS THE PAGE FROM REFRESHING AFTER CLICKING 'GET FORECAST' BUTTON
+    e.preventDefault();
+
     fetch(apiUrl)
     // getForecast CONVERTS THE RESPONSE INTO A JSON OBJECT 
     .then(response => response.json())
@@ -28,7 +38,54 @@ const Forecast = () => {
 
         <h2>Find Current Weather Conditions</h2>
 
-        <button onClick={getForecast}>Get Forecast</button>
+        <form onSubmit={getForecast}>
+
+            <input
+
+                type="text"
+                placeholder="Enter City"
+                maxLength="50"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+
+            />
+
+            <label>
+
+                <input
+
+                    type="radio"
+                    name="units"
+                    checked={unit === "imperial"}
+                    value="imperial"
+                    onChange={(e) => setUnit(e.target.value)}
+
+                />
+
+                Fahrenheit
+
+            </label>
+
+            <label>
+
+                <input
+
+                    type="radio"
+                    name="units"
+                    checked={unit === "metric"}
+                    value="metric"
+                    onChange={(e) => setUnit(e.target.value)}
+
+                />
+
+                Celcius
+
+            </label>
+
+            <button type="submit">Get Forecast</button>
+
+        </form>
+
 
         <Conditions 
 
