e30f5f81a3ca14f6dd78e816778ff3e1a6a12dc3
Catalina Williams <webdevcatalina@gmail.com>
2021-07-16 15:26:35 -0600

    added Conditions component to Forecast component. Applications functions correctly

--- client/src/components/Forecast/Forecast.js
+++ client/src/components/Forecast/Forecast.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import Conditions from '../Conditions/Conditions';
 
 const Forecast = () => {
 
+    //EXAMPLE responseObj FOR THE CITY OF DENVER
+    //{"coord":{"lon":-104.9847,"lat":39.7392},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":305.65,"feels_like":303.49,"temp_min":296.88,"temp_max":307.95,"pressure":1009,"humidity":18},"visibility":10000,"wind":{"speed":0.45,"deg":69,"gust":1.34},"clouds":{"all":100},"dt":1626468937,"sys":{"type":2,"id":2004334,"country":"US","sunrise":1626435920,"sunset":1626488779},"timezone":-21600,"id":5419384,"name":"Denver","cod":200}
     let [responseObj, setResponseObj] = useState({});
 
     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=denver&appid=e85d65f27c4dba6f1470e1cee950c874`
@@ -25,11 +28,13 @@ const Forecast = () => {
 
         <h2>Find Current Weather Conditions</h2>
 
-        <div>
-            {JSON.stringify(responseObj)}
-        </div>
-
         <button onClick={getForecast}>Get Forecast</button>
+
+        <Conditions 
+
+            responseObj={responseObj}
+        
+        />
 
     </div>
 
